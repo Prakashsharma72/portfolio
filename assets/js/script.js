2ee96345fba4ec13e073733379bb31dd8adf57d5
prakash sharma <129486882+Prakashsharma72@users.noreply.github.com>
2023-04-20 17:52:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Ravi Sharma";
+            document.title = "Portfolio | Prakash Sharma";
             $("#favicon").attr("href", "assets/images/pofile3-.png");
         }
         else {
